9f1f114635b9bb96f8dcb1bdbf2d3bd041330e32
nang-ying-lao-hsaing <122967319+nang-ying-lao-hsaing@users.noreply.github.com>
2023-11-26 10:41:45 +0000

    Note

---
+++ src/main/java/th/mfu/domain/Note.java
@@ -0,0 +1,63 @@
+package th.mfu.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String text;
+
+    public Note() {
+
+    }
+
+    public Note(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
