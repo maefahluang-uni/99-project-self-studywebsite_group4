47db9f55aa44b5e9e1fb9bc595ce2f2b4a3ed77d
Thomas MMK <sawthomas2020@gmail.com>
2023-11-26 21:52:56 +0700

    Flashcard

--- src/main/java/th/mfu/controller/NoteController.java
+++ src/main/java/th/mfu/controller/NoteController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import antlr.collections.List;
 import th.mfu.domain.Note;
 import th.mfu.repository.NoteRepository;
 
@@ -32,7 +34,7 @@ public class NoteController {
     @GetMapping("/notes")
     public String listNote(Model model) {
         model.addAttribute("notes", noteRepo.findAll());
-        return "notes";
+        return "note-search";
     }
 
     // Handler method for deleting a note
@@ -41,4 +43,27 @@ public class NoteController {
         noteRepo.deleteById(id);
         return "redirect:/notes";
     }
+
+    // @GetMapping("/user-search-tea")
+    // public String searchTea(Model model) {
+    //     // model.addAttribute("dishes", dishesRepo.findByDishtype("tea"));
+    //     Iterable<Dishes> activeTeaDishes = dishesRepo.findByDishtypeAndDishStatus("tea", "active");
+    //     model.addAttribute("dishes", activeTeaDishes);
+
+    //     // only show InvoiceItem that invoice = null
+    //     model.addAttribute("invoiceitem", invoiceItemRepo.findByInvoiceIsNull());
+    //     return "user";
+    // }
+
+    // @PostMapping("/notes/search")
+    // public String searchNotes(@RequestParam(name = "query", required = false) String query, Model model) {
+    //     // Perform the search using your service
+    //     Iterable<Note> searchResults = noteRepo.searchNotes(query);
+
+    //     // Add the search results to the model
+    //     model.addAttribute("searchResults", searchResults);
+
+    //     return "search-results"; // Thymeleaf template name: "searchResults.html"
+    // }
+
 }
