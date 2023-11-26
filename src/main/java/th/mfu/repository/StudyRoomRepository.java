c36912e70213e8301972a38ccf1f0915463ace82
Thomas MMK <sawthomas2020@gmail.com>
2023-11-26 23:58:55 +0700

    final

--- src/main/java/th/mfu/repository/StudyRoomRepository.java
+++ src/main/java/th/mfu/repository/StudyRoomRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import th.mfu.domain.StudyRoom;
 
 public interface StudyRoomRepository extends CrudRepository<StudyRoom, Long>{
-    
+      StudyRoom findByName(String name);
 }
